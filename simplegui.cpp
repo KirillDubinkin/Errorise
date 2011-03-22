81c12b3db0813e584c879c4d648779eb959780a4
we_apon <kirill.aka.weapon@gmail.com>
2011-03-22 22:35:53 +0300

    Shutted up warning in SimpleGUI, about adding already added layout

--- simplegui.cpp
+++ simplegui.cpp
@@ -9,27 +9,23 @@
 SimpleGUI::SimpleGUI(QWidget *parent) :
     QDialog(parent)
 {
-    mainLayout = new QVBoxLayout(this);
+    mainLayout = new QVBoxLayout();
 
-    toolbar = new SimpleToolbar(this);
-    pl = new SimplePlaylist(this);
-//    alist = new SimpleAlbumlist(this);
-    tree = new AlbumTree(this);
+    toolbar = new SimpleToolbar();
+    pl      = new SimplePlaylist();
+    tree    = new AlbumTree();
 
 
-    QHBoxLayout *hl = new QHBoxLayout(this);
-    //hl->addWidget(alist, 30);
+    QHBoxLayout *hl = new QHBoxLayout();
     hl->addWidget(tree, 34);
     hl->addWidget(pl, 70);
 
-    this->mainLayout->setMenuBar(toolbar);
-    this->mainLayout->addLayout(hl, 100);
+    mainLayout->setMenuBar(toolbar);
+    mainLayout->addLayout(hl, 100);
 
     this->setLayout(this->mainLayout);
     this->setGeometry(pref->x, pref->y, pref->res_main_width, pref->res_main_height);
 
-
-    //connect(alist, SIGNAL(directoryChanged(QString)), mediainfo, SLOT(scanDir(QString)));
 }
 
 SimpleGUI::~SimpleGUI()
