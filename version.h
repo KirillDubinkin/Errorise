087764bf54691734f465b10389b945e3b9b3a694
we_apon <weapon@lamer-pc.(none)>
2011-04-25 03:23:59 +0400

    Test one-two

--- version.h
+++ version.h
@@ -2,12 +2,17 @@
 #define _VERSION_H_
 
 #define NAME "Errorise"
-#define VERSION "0.1.2"
+#define VERSION "0.1.2.242"
 #define COMPANY "we_apon"
 
 #include <QString>
+#include <QTime>
+#include <QApplication>
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
 
-static inline QString myplayerVersion() { return QString(VERSION); }
+static inline QString myplayerVersion() { return QString(VERSION + QFileInfo(qApp->applicationFilePath()).lastModified().toString("   ddd, dd.MM.yyyy hh:mm") ); }
 static inline QString myplayerName() { return QString(NAME); }
 static inline QString mycompany() { return QString(COMPANY); }
 
