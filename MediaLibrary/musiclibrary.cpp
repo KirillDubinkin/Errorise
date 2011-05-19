4c4947edbf609d4c15d0bea71d62c5e0220180ac
we_apon <kirill.aka.weapon@gmail.com>
2011-05-20 01:08:22 +0400

    Set MediaInfo as tag-scanner on linux too It still better, than using Phonon

--- MediaLibrary/musiclibrary.cpp
+++ MediaLibrary/musiclibrary.cpp
@@ -1,11 +1,7 @@
 #include "musiclibrary.h"
 #include "global.h"
 
-#ifdef Q_OS_LINUX
-#include "pmediainfo.h"
-#else
 #include "mediainfo.h"
-#endif
 
 #include <QDir>
 #include <QtSql/QSqlQuery>
@@ -38,11 +34,7 @@ MusicLibrary::MusicLibrary(const QString &libPath, const QString &filters,
 
     createTagsTable();
 
-#ifdef Q_OS_LINUX
-    minfo = new PMediaInfo(this);
-#else
     minfo = new MediaInfo(this);
-#endif
 
     connect(minfo, SIGNAL(newFilesScanned(QMultiMap<QString,QMultiMap<QString,QString> >)),
             this, SLOT(insertNewTracks(QMultiMap<QString,QMultiMap<QString,QString> >)));
